b012bb2316becbbbe1c5c38f6a807f61bac96003
Hugo Viana <hugo@viana.fr>
2026-09-23 14:35:16 +0200

    first commit

---
+++ src/test/java/com/lesageviana/email_manager/EmailManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesageviana.email_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
